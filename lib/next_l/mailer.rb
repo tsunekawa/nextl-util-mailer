43f43bec8f05a3ec018fbafb2bdca9b665dc0737
Mao Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2012-04-26 19:17:14 +0900

    refactoring NextL::Mailer and NextL::Mail

--- lib/next_l/mailer.rb
+++ lib/next_l/mailer.rb
@@ -1,38 +1,74 @@
 #-*- coding:utf-8 -*-
 
 # ja: NextLのメーリングリストにメールを送るモジュール
-module NextL::Mailer
+class NextL::Mailer
+
+  def initialize
+    @mailconf = ::NextL::Config[:mail]
+  end
+
   # ja: enju_leaf に投稿された Issue のメールを作成する
-  def self.issues(opts={})
+  def issues(opts={})
     limit = (opts[:limit] || -1).to_i
-    mailconf = ::NextL::Config[:mail]
     issues   = ::NextL::EnjuRepo.issues[0..limit].reverse
 
     issues.map do |event|
-      issue    = event["payload"]["issue"]
-      body_str = case event["type"]
-        when "IssuesEvent"
-          issue["body"]
-        when "IssueCommentEvent"
-          event["payload"]["comment"]["body"]
-        else
-          raise
+      mail = ::NextL::Mail.new do |mail|
+        mail.from @mailconf[:recipient]
+        mail.to @mailconf[:to]
       end
-
-      Mail.new do
-        from    mailconf[:recipient]
-        to      mailconf[:to]
-        subject "issue #{issue["number"]} - #{issue["title"]}"
-        body    body_str
-      end
+      mail.render_by_event(event)
     end
   end
 
   # ja: 取得した Issue をすべて転送する
-  def self.deliver_issues(opts={})
-    self.issues(opts).each{|mail|
+  def deliver_issues(opts={})
+    issues(opts).each{|mail|
       mail.deliver!
       puts mail.subject
     }
+  end
+end
+
+# ja: NextLのMLに送るメールのオブジェクト
+class NextL::Mail
+  def initialize(&block)
+    @mail = ::Mail.new
+    yield @mail
+  end
+
+  # ja: GitHubのイベントに応じてタイトルと本文を変更する
+  def render_by_event(event)
+    type = case event["type"]
+      when "IssuesEvent"
+        render_issue(event)
+      when "IssueCommentEvent"
+        render_issues_comment(event)
+      else
+        raise
+    end
+    self
+  end
+
+  # ja: IssuesEventの場合の件名と本文をレンダリング
+  def render_issues(event)
+    issue    = event["payload"]["issue"]
+    @mail.subject "issue #{issue["number"]} - #{issue["title"]}"
+    @mail.body    event["payload"]["issue"]["body"]
+  end
+
+  # ja: IssueCommentの場合の件名と本文をレンダリング
+  def render_issues_comment(event)
+    issue    = event["payload"]["issue"]
+    @mail.subject "issue #{issue["number"]} - #{issue["title"]}"
+    @mail.body    event["payload"]["comment"]["body"]
+  end
+
+  # ja: メールの送受信に関する処理は@mailに委譲する
+  def method_missing(methodname, *args)
+    if @mail.respond_to? methodname
+      @mail.send(methodname, *args)
+      self
+    end
   end
 end
